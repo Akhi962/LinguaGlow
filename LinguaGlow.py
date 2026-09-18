7d7a1e11f427d9c7f8ddcc5079d90035b23295f5
Akhilesh Mahendra <mahendraakhilesh18@gmail.com>
2026-09-18 12:37:50 +0530

    Update LinguaGlow.py

--- LinguaGlow.py
+++ LinguaGlow.py
@@ -66,7 +66,7 @@ with tab1:
             return None
         try:
             genai.configure(api_key = api_key)
-            model = genai.GenerativeModel('gemini-2.5-flash')
+            model = genai.GenerativeModel('gemini-3.6-flash')
             response = model.generate_content(prompt_text)
             return response.text
         except Exception as e:
